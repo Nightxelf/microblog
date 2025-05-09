b85b8bfe839da1464c74ad98c1ffbd1524f1a413
Nightxelf <alienhunterchess@gmail.com>
2025-05-09 15:40:45 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))  
 
 class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))  # basedir is a class attribute
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
