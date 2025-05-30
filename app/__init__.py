ce70ae583d297f6969144b084129243da355adad
Nightxelf <alienhunterchess@gmail.com>
2025-05-30 22:22:51 +0530

    till ch 10

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,14 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
 
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
+mail = Mail(app)
 # db.init_app(app)
 migrate = Migrate(app, db)
 
